831d6575eb36c51cb42f61e9a8c21564bda513c9
Sofi V. Curcio <79474103+sofivc1998@users.noreply.github.com>
2025-03-27 18:29:41 -0300

    Add tailwind.config.js 1743110980941

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'adidas-black': '#000000',
+        'adidas-white': '#FFFFFF',
+        'adidas-red': '#ED1C24',
+        'adidas-green': '#006341'
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+}
